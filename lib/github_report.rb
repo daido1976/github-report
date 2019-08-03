0887af2915c96ef41cdab64205d9c706459debf8
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-08-04 06:54:35 +0900

    [Refactor] Fix comment and Change position

--- lib/github_report.rb
+++ lib/github_report.rb
@@ -4,12 +4,14 @@ require 'octokit'
 require 'pry'
 
 class GithubReport
+  # @param from [String] (e.g. '2019-01-01')
+  # @param to [String] (e.g. '2019-01-31')
   def initialize(from: Date.today.to_s, to: Date.today.to_s)
-    @client = Octokit::Client.new(access_token: ENV['GITHUB_REPORT_ACCESS_TOKEN'])
     # contributionsCollection の args として渡す Datetime は iso8601 に則った形式にしないといけないためこうしている
     # see https://developer.github.com/v4/object/user/#contributionscollection
     @from_date = "#{from}T00:00:00+09:00"
     @to_date = "#{to}T00:00:00+09:00"
+    @client = Octokit::Client.new(access_token: ENV['GITHUB_REPORT_ACCESS_TOKEN'])
   end
 
   def list
@@ -40,16 +42,16 @@ class GithubReport
 
   # @param contributions [Hash]
   # @param type [Symbol] :issue or :pullRequest
-  # @return [Hash<Array>] Value の Array をレポジトリ名でグルーピングした Hash を返す
+  # @return [Hash<Symbol, Array>] レポジトリ名でグルーピングした Hash を返す
   def group_edges(contributions, type:)
     contributions.send("#{type}Contributions".to_sym).edges.group_by do |edge|
       edge.node.send(type).repository.nameWithOwner
     end
   end
 
-  # @param grouped_issue [Hash<Array>]
-  # @param grouped_pr [Hash<Array>]
-  # @return [Hash<Array>]
+  # @param grouped_issue [Hash<Symbol, Array>]
+  # @param grouped_pr [Hash<Symbol, Array>]
+  # @return [Hash<Symbol, Array>]
   def merge_issue_and_pr(grouped_issue, grouped_pr)
     grouped_issue.merge(grouped_pr) do |_, issue_val, pr_val|
       issue_val + pr_val
@@ -57,7 +59,7 @@ class GithubReport
   end
 
   # Issue と PR のリストを標準出力に出力する
-  # @param issue_and_pr [Hash<Array>]
+  # @param issue_and_pr [Hash<Symbol, Array>]
   # @return [void]
   def puts_list(issue_and_pr)
     issue_and_pr.each do |title, values|
